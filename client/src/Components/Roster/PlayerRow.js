92882740a9dc689253902720b24ffdcb7e743da6
unknown <steve@gridtop.com>
2020-05-27 12:59:56 -0500

    fixed issues and added behavior for touch handler

--- client/src/Components/Roster/PlayerRow.js
+++ client/src/Components/Roster/PlayerRow.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 const PlayerRow = props => {
+  const [ showRow, setShowRow ] = React.useState(false);
+  const [ viewWidth, setViewWidth ] = React.useState(window.innerWidth);
+
+  React.useEffect(() => { if (!viewWidth) { setViewWidth(window.innerWidth); }}, [viewWidth]);
+
+  const handleResize = () => {
+    setViewWidth(window.innerWidth);
+  };
+
+  window.addEventListener('resize', handleResize);
   /* 
     Name, Position, Archtype, Ovr, Yr, Ht, Wt, St,
     HS/JC, Pot, Num
@@ -11,23 +21,40 @@ const PlayerRow = props => {
     props.toggle();
     props.getData(data);
   };
-  return (
-    <tr>
-      <th className="clickable" onClick={toggleModal}>
-        {props.data.name}
-      </th>
-      <td label="Position">{props.data.position}</td>
-      <td label="Type">{props.data.archtype}</td>
-      <td label="Overall">{props.data.overall}</td>
-      <td label="Year">{props.data.year}</td>
-      <td label="Height">{props.data.height}</td>
-      <td label="Weight">{props.data.weight}</td>
-      <td label="State">{props.data.state}</td>
-      <td label="School">{props.data.school}</td>
-      <td label="Potential">{props.data.potential}</td>
-      <td label="Number">{props.data.jersey}</td>
-    </tr>
-  );
-};
+  if (showRow || viewWidth >= 901) {
+    return (
+          <tr onTouchEnd={(e) => {
+              if (e.cancelable) { e.preventDefault(); }
+              setShowRow(!showRow);
+            }} >
+            <th className="clickable" onClick={toggleModal}>
+              {props.data.name}
+            </th>
+                <td label="Position">{props.data.position}</td>
+                <td label="Type">{props.data.archtype}</td>
+                <td label="Overall">{props.data.overall}</td>
+                <td label="Year">{props.data.year}</td>
+                <td label="Height">{props.data.height}</td>
+                <td label="Weight">{props.data.weight}</td>
+                <td label="State">{props.data.state}</td>
+                <td label="School">{props.data.school}</td>
+                <td label="Potential">{props.data.potential}</td>
+                <td label="Number">{props.data.jersey}</td>
+          </tr>
+    );
+  } else {
+    return (
+      <tr onTouchEnd={(e) => {
+        if (e.cancelable) { e.preventDefault(); }
+        setShowRow(!showRow);
+      }} >
+        <th className="clickable" onClick={toggleModal}>
+          {props.data.name}
+        </th>
+      </tr>
+    );
+  };
+
+}
 
 export default PlayerRow;
