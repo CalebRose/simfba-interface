046c87a56f1c02869f586b0c4bf069ca9bb26d5f
CalebRose <ctrose17@gmail.com>
2020-08-08 13:51:00 -0500

    Resetting storage upon logout

--- src/Components/NavBar/AuthorizedUser.js
+++ src/Components/NavBar/AuthorizedUser.js
@@ -1,47 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { auth } from "../../Firebase/firebase";
-import routes from "../../Constants/routes";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../../Firebase/firebase';
+import routes from '../../Constants/routes';
 
-const authorizedUser = props => {
+const authorizedUser = (props) => {
   const { user } = props;
-  var RoleList = props => {
+  var RoleList = (props) => {
     if (props.user.roleID === 1) {
       return (
-        <div className="navbar-item has-dropdown">
+        <div className='navbar-item has-dropdown'>
           <a
-            href="#"
-            className="navbar-link"
-            data-toggle="dropdown"
-            role="button"
-            aria-expanded="false"
+            href='#'
+            className='navbar-link'
+            data-toggle='dropdown'
+            role='button'
+            aria-expanded='false'
           >
-            <span className="glyphicon glyphicon-info-sign"></span>
-            Administration <span className="caret"></span>
+            <span className='glyphicon glyphicon-info-sign'></span>
+            Administration <span className='caret'></span>
           </a>
-          <div className="navbar-dropdown">
-            <a className="navbar-item" href="/admin/teams/all">
+          <div className='navbar-dropdown'>
+            <a className='navbar-item' href='/admin/teams/all'>
               All Teams
             </a>
-            <hr className="navbar-divider"></hr>
-            <a href="/admin/teams/assign">Assign Team</a>
+            <hr className='navbar-divider'></hr>
+            <a href='/admin/teams/assign'>Assign Team</a>
           </div>
         </div>
       );
     } else return null;
   };
 
+  const logout = () => {
+    auth.signOut();
+    localStorage.removeItem('token');
+  };
+
   return (
-    <div className="navbar-end">
-      <div className="navbar-item">
+    <div className='navbar-end'>
+      <div className='navbar-item'>
         <Link to={routes.USER}>
-          <span className="glyphicon glyphicon-user"></span>
+          <span className='glyphicon glyphicon-user'></span>
           {user.username}
         </Link>
       </div>
-      <div className="navbar-item">
-        <Link to={routes.LANDING} onClick={() => auth.signOut()}>
-          <span className="fas fa-sign-out-alt"></span> log out
+      <div className='navbar-item'>
+        <Link to={routes.LANDING} onClick={logout}>
+          <span className='fas fa-sign-out-alt'></span> log out
         </Link>
       </div>
       <RoleList user={user} />
