90283effd37cd68c59e2ec9e5eb3ad8add6be923
CalebRose <ctrose17@gmail.com>
2021-05-10 11:22:27 -0700

    CSS icon adjustments

--- src/Components/Admin/ApproveRequests/RequestRow.js
+++ src/Components/Admin/ApproveRequests/RequestRow.js
@@ -1,47 +1,47 @@
 import React from 'react';
 
 const RequestRow = (props) => {
-  let data = props.request;
+    let data = props.request;
 
-  const approve = () => {
-    const payload = {
-      username: data.Username,
-      team: data.Team,
-      mascot: data.Nickname,
-      teamAbbr: data.Abbr,
-      teamId: data.TeamId,
-      reqId: data.id,
+    const approve = () => {
+        const payload = {
+            username: data.Username,
+            team: data.Team,
+            mascot: data.Nickname,
+            teamAbbr: data.Abbr,
+            teamId: data.TeamId,
+            reqId: data.id
+        };
+        return props.approve(payload);
     };
-    return props.approve(payload);
-  };
 
-  const reject = () => {
-    return props.reject({ reqId: data.TeamId, id: data.id });
-  };
+    const reject = () => {
+        return props.reject({ reqId: data.TeamId, id: data.id });
+    };
 
-  return (
-    <tr>
-      <td>{data.Team + ' ' + data.Nickname}</td>
-      <td>{data.Current_Conference}</td>
-      <td>{data.Username}</td>
-      <td>
-        <button
-          className='button is-rounded is-success is-outlined'
-          onClick={approve}
-        >
-          <span className='request-icon'>&#10003;</span>
-        </button>
-      </td>
-      <td>
-        <button
-          className='button is-rounded request is-danger is-outlined'
-          onClick={reject}
-        >
-          <span className=''>&#215;</span>
-        </button>
-      </td>
-    </tr>
-  );
+    return (
+        <tr>
+            <td>{data.Team + ' ' + data.Nickname}</td>
+            <td>{data.Current_Conference}</td>
+            <td>{data.Username}</td>
+            <td>
+                <h2>
+                    <i
+                        className="bi bi-check-circle rounded-circle link-success"
+                        onClick={approve}
+                    ></i>
+                </h2>
+            </td>
+            <td>
+                <h2>
+                    <i
+                        className="bi bi-x-circle rounded-circle link-danger"
+                        onClick={reject}
+                    ></i>
+                </h2>
+            </td>
+        </tr>
+    );
 };
 
 export default RequestRow;
--- src/Components/Admin/ManageTeams/TeamRow.js
+++ src/Components/Admin/ManageTeams/TeamRow.js
@@ -1,34 +1,34 @@
 import React from 'react';
 
 const TeamRow = (props) => {
-  let data = props.team;
+    let data = props.team;
 
-  const revoke = () => {
-    const payload = {
-        username: data.Coach,
-        team: data.Team,
-        mascot: data.Nickname,
-        teamAbbr: data.Abbr,
-        reqId: data.id,
-      };
-    return props.revoke(payload);
-  };
+    const revoke = () => {
+        const payload = {
+            username: data.Coach,
+            team: data.Team,
+            mascot: data.Nickname,
+            teamAbbr: data.Abbr,
+            reqId: data.id
+        };
+        return props.revoke(payload);
+    };
 
-  return (
-    <tr>
-      <td>{data.Team + ' ' + data.Nickname}</td>
-      <td>{data.Coach}</td>
-      <td>{data.Current_Conference}</td>
-      <td>
-        <button
-          className='button is-rounded request is-danger is-outlined'
-          onClick={revoke}
-        >
-          <span className=''>&#215;</span>
-        </button>
-      </td>
-    </tr>
-  );
+    return (
+        <tr>
+            <td>{data.Team + ' ' + data.Nickname}</td>
+            <td>{data.Coach}</td>
+            <td>{data.Current_Conference}</td>
+            <td>
+                <h2>
+                    <i
+                        className="bi bi-x-circle rounded-circle link-danger"
+                        onClick={revoke}
+                    ></i>
+                </h2>
+            </td>
+        </tr>
+    );
 };
 
 export default TeamRow;
