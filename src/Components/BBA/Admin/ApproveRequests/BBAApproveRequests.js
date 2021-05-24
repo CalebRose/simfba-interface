6905cc31b46dfdc669942cf68a58fbb86415750e
CalebRose <ctrose17@gmail.com>
2021-05-23 20:30:04 -0700

    Resolved issue where admin could not approve of teams

--- src/Components/BBA/Admin/ApproveRequests/BBAApproveRequests.js
+++ src/Components/BBA/Admin/ApproveRequests/BBAApproveRequests.js
@@ -1,70 +1,78 @@
 import React, { useEffect } from 'react';
-import { connect, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import firebase from 'firebase';
 import SimBBA_url from '../../../../Constants/SimBBA_url';
-import RequestRow from '../../../Admin/ApproveRequests/RequestRow';
 import BBARequestService from '../../../../_Services/simNBA/BBARequestService';
+import BBARequestRow from './BBARequestRow';
 
 const BBAApproveRequests = ({ currentUser }) => {
     // State
-    const user = useSelector((state) => state.user.currentUser);
     const [requests, setRequests] = React.useState([]);
     const requestService = new BBARequestService();
     // Use Effects Begin
     // Get Requests
     useEffect(() => {
-        const getRequests = async () => {
-            let requests = requestService.GetRequests(SimBBA_url);
-            setRequests(requests);
-        };
-        if (user) {
+        if (currentUser) {
             getRequests();
         }
-    }, [user]);
+    }, [currentUser]);
 
     // Use Effects End
+    const getRequests = async () => {
+        let requests = await requestService.GetTeamRequests(SimBBA_url);
+        setRequests(requests);
+    };
 
     // Click Functions
     const approveRequest = async (payload) => {
         try {
             // DB request for Request
-            let res = await requestService.ApproveRequest(SimBBA_url, payload);
+            let requestPayload = {
+                ID: payload.reqId,
+                TeamID: payload.cbb_id,
+                Username: payload.Username,
+                IsApproved: true
+            };
+
+            console.log(requestPayload);
+
+            let res = await requestService.ApproveRequest(
+                SimBBA_url,
+                requestPayload
+            );
 
             if (res.ok) {
                 console.log(
                     'Approved Request:',
-                    payload.teamId + ' ' + payload.team + ' ' + payload.username
+                    payload.reqId + ' ' + payload.Team + ' ' + payload.Username
                 );
-            } else {
-                throw ('HTTP-Error: Approval incomplete', res.status);
-            }
-
-            // DB Request for assigning coach to team
-            let assignTeam = await requestService.AssignTeam(
-                SimBBA_url,
-                payload,
-                res
-            );
-
-            if (assignTeam.ok) {
                 console.log(
-                    payload.username + ' is now assigned to ' + payload.team
+                    payload.Username + ' is now assigned to ' + payload.Team
                 );
             } else {
                 throw ('HTTP-Error: Approval incomplete', res.status);
             }
+
+            const firebasePayload = {
+                cbb_team: payload.cbb_team,
+                cbb_abbr: payload.cbb_abbr,
+                cbb_id: payload.cbb_id,
+                reqId: payload.reqId
+            };
 
             // Firebase Call
             const firestore = firebase.firestore();
             let userRef = await firestore
                 .collection('users')
-                .where('username', '==', payload.username)
+                .where('username', '==', payload.Username)
                 .get();
 
             userRef.forEach(async (doc) => {
-                await doc.ref.update(payload);
+                await doc.ref.update(firebasePayload);
             });
+
             console.log('Firebase Updated');
+
             // Filter Requests
             const filterRequests = requests.filter(
                 (x) => x.id !== payload.reqId
@@ -102,19 +110,22 @@ const BBAApproveRequests = ({ currentUser }) => {
                     <h2 className="title is-3">SimBBA Approve Requests</h2>
                     <div className="availableScrollbar available-ui-height availableTeams">
                         <div className="table-wrapper dTable">
-                            <table className="table is-fullwidth is-hoverable is-truncated">
+                            <table className="table table-hover">
                                 <thead>
                                     <tr>
-                                        <th style={{ width: '120px' }}>
+                                        <th scope="col">
                                             <abbr>Team</abbr>
                                         </th>
-                                        <th style={{ width: '120px' }}>
+                                        <th scope="col">
                                             <abbr>Conference</abbr>
                                         </th>
-                                        <th style={{ width: '80px' }}>
+                                        <th scope="col">
                                             <abbr title="User">User</abbr>
                                         </th>
-                                        <th style={{ width: '50px' }}>
+                                        <th scope="col">
+                                            <abbr title="League">League</abbr>
+                                        </th>
+                                        <th scope="col">
                                             <abbr title="Approve">Approve</abbr>
                                         </th>
                                         <th style={{ width: '50px' }}>
@@ -124,16 +135,19 @@ const BBAApproveRequests = ({ currentUser }) => {
                                 </thead>
                                 <tfoot>
                                     <tr>
-                                        <th style={{ width: '200px' }}>
+                                        <th scope="col">
                                             <abbr>Team</abbr>
                                         </th>
-                                        <th style={{ width: '120px' }}>
+                                        <th scope="col">
                                             <abbr>Conference</abbr>
                                         </th>
-                                        <th style={{ width: '80px' }}>
+                                        <th scope="col">
                                             <abbr title="User">User</abbr>
                                         </th>
-                                        <th style={{ width: '50px' }}>
+                                        <th scope="col">
+                                            <abbr title="League">League</abbr>
+                                        </th>
+                                        <th scope="col">
                                             <abbr title="Approve">Approve</abbr>
                                         </th>
                                         <th style={{ width: '50px' }}>
@@ -142,16 +156,18 @@ const BBAApproveRequests = ({ currentUser }) => {
                                     </tr>
                                 </tfoot>
                                 <tbody>
-                                    {requests.map((x, i) => {
-                                        return (
-                                            <RequestRow
-                                                key={i}
-                                                request={x}
-                                                approve={approveRequest}
-                                                reject={rejectRequest}
-                                            />
-                                        );
-                                    })}
+                                    {requests.length > 0
+                                        ? requests.map((x, i) => {
+                                              return (
+                                                  <BBARequestRow
+                                                      key={i}
+                                                      request={x}
+                                                      approve={approveRequest}
+                                                      reject={rejectRequest}
+                                                  />
+                                              );
+                                          })
+                                        : ''}
                                 </tbody>
                             </table>
                         </div>
---
+++ src/Components/BBA/Admin/ApproveRequests/BBARequestRow.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const BBARequestRow = (props) => {
+    let data = props.request;
+
+    const approve = () => {
+        let payload = {};
+        if (data.IsNBA === false) {
+            payload = {
+                Username: data.Username,
+                Conference: data.Conference,
+                IsApproved: true,
+                cbb_team: data.Team,
+                cbb_abbr: data.Abbr,
+                cbb_id: data.TeamID,
+                reqId: data.ID,
+                Team: data.Team
+            };
+        }
+
+        return props.approve(payload);
+    };
+
+    const reject = () => {
+        return props.reject({ reqId: data.TeamId, id: data.id });
+    };
+
+    return (
+        <tr>
+            <td>{data.Team}</td>
+            <td>{data.Conference}</td>
+            <td>{data.Username}</td>
+            <td>{data.IsNBA ? 'NBA' : 'College'}</td>
+            <td>
+                <h2>
+                    <i
+                        className="bi bi-check-circle rounded-circle link-success"
+                        onClick={approve}
+                    ></i>
+                </h2>
+            </td>
+            <td>
+                <h2>
+                    <i
+                        className="bi bi-x-circle rounded-circle link-danger"
+                        onClick={reject}
+                    ></i>
+                </h2>
+            </td>
+        </tr>
+    );
+};
+
+export default BBARequestRow;
