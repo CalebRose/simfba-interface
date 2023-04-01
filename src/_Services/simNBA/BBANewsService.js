6ff4e13fa9ffd64de1507dca41b15fdaadd0fcbf
Anna <annahuang068@gmail.com>
2023-04-01 09:34:58 -0400

    added in NFL category, modified selected seasons, and updated newstypelist with NFL types

--- src/_Services/simNBA/BBANewsService.js
+++ src/_Services/simNBA/BBANewsService.js
@@ -1,19 +1,27 @@
 import url from '../../Constants/SimBBA_url';
 
 export default class BBANewsService {
-    async GetAllNewsLogsForASeason(season) {
-        let json;
-        let response = await fetch(url + 'cbb/news/all/' + season, {
-            headers: {
-                authorization: 'Bearer ' + localStorage.getItem('token')
-            }
-        });
+    async GetAllNewsLogsForACbbSeason(cbbSeason) {
+        //this uses GetCall function from FetchHelper file to return news for a CBB season
+        return GetCall(`${url}cbb/news/all/`);
+        //Ignore code below as created selected season
+        // let json;
+        // let response = await fetch(url + 'cbb/news/all/' + season, {
+        //     headers: {
+        //         authorization: 'Bearer ' + localStorage.getItem('token')
+        //     }
+        // });
 
-        if (response.ok) {
-            json = await response.json();
-        } else {
-            alert('HTTP-Error:', response.status);
-        }
-        return json;
+        // if (response.ok) {
+        //     json = await response.json();
+        // } else {
+        //     alert('HTTP-Error:', response.status);
+        // }
+        // return json;
+    }
+
+    async GetAllNewsLogsForANbaSeason(nbaSeason) {
+        //this uses GetCall function from FetchHelper file to return news for a NBA season
+        return GetCall(`${url}nba/news/all/`);
     }
 }
